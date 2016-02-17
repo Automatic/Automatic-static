106d1206e7443f6ba1687b5199d8e5f237ac6c5e
Yanglin Zhao <yz1876@gmail.com>
2016-02-17 14:53:52 -0500

    Add aws s3 deploy target
    Summary: make deploy should deploy to s3
    
    Test Plan: make lint, make test, make deploy
    
    Reviewers: aaronnorby, ian.gilman, ruthie
    
    Reviewed By: ruthie
    
    Differential Revision: https://phabricator.automatic.co/D11891

--- tools/gulp/deploy.js
+++ tools/gulp/deploy.js
@@ -1,12 +1,18 @@
-import { isEmpty } from 'lodash';
+import assert from 'assert';
+
+import { isNumber } from 'lodash';
 import gulp from 'gulp';
 import ghPages from 'gulp-gh-pages';
+import awsPublish from 'gulp-awspublish';
 
 import config from '../../config.js';
 
 function deployGithub() {
-  if (isEmpty(config.DEPLOY_GITHUB_REMOTE_URL)) {
-    throw new Error('Missing DEPLOY_GITHUB_REMOTE_URL when attempting to deploy to github pages');
+  try {
+    assert(config.DEPLOY_GITHUB_REMOTE_URL);
+  } catch (err) {
+    console.error('Missing github deploy environmental variables');
+    process.exit(1);
   }
 
   return gulp.src('dist/**')
@@ -15,11 +21,64 @@ function deployGithub() {
     }));
 }
 
+function deployAwsGlobs(artifactsGlob, cacheLength) {
+  assert(isNumber(cacheLength));
+  try {
+    assert(config.DEPLOY_AWS_BUCKET_NAME);
+    assert(config.DEPLOY_AWS_ACCESS_KEY);
+    assert(config.DEPLOY_AWS_SECRET_KEY);
+    assert(config.DEPLOY_AWS_REGION);
+  } catch (err) {
+    console.error('Missing AWS deploy environmental variables');
+    process.exit(1);
+  }
+  const publisherConf = {
+    params: {
+      Bucket: config.DEPLOY_AWS_BUCKET_NAME
+    },
+    accessKeyId: config.DEPLOY_AWS_ACCESS_KEY,
+    secretAccessKey: config.DEPLOY_AWS_SECRET_KEY,
+    region: config.DEPLOY_AWS_REGION
+  };
+
+  const publisher = awsPublish.create(publisherConf);
+  const headers = {
+    'Cache-Control': `max-age=${cacheLength} no-transform, public`
+  };
+  return gulp.src(artifactsGlob)
+    .pipe(publisher.publish(headers, { force: true }))
+    .pipe(awsPublish.reporter({}));
+}
+
+function deployAws() {
+  const longTermCachePatterns = [
+    '*.html',
+    'favicon.ico',
+    'apple-touch-icon.png',
+    'apple-touch-icon-precomposed.png',
+    'robots.txt',
+    'sitemap.xml'
+  ];
+
+  const oneYear = 31536000;
+  const longTermCacheGlob = `dist/**/+(${longTermCachePatterns.join('|')})`;
+  const longTermDeployStream = deployAwsGlobs(longTermCacheGlob, oneYear);
+
+  const fiveMin = 300;
+  const shortTermCacheGlob = `dist/**/!(${longTermCachePatterns.join('|')}`;
+  const shortTermDeploySteam = deployAwsGlobs(shortTermCacheGlob, fiveMin);
+
+  return gulp.parallel(longTermDeployStream, shortTermDeploySteam);
+}
+
 function deploy() {
   let deployStream;
   switch (config.DEPLOY_TARGET) {
     case 'github':
       deployStream = deployGithub();
+      break;
+    case 'aws':
+      deployStream = deployAws();
       break;
     default:
       throw new Error('Invalid deploy target');
