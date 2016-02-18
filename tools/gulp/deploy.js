6550bd5c49a44f1c262a0f9b5ef197b812ea773f
Yanglin Zhao <yz1876@gmail.com>
2016-02-18 13:27:31 -0500

    Fix aws deployment gulp scripts
    Summary: No time to fix github and s3 deploy. Just gonna opt to make it s3 only, and add back github when I have time
    
    Test Plan: make deploy
    
    Reviewers: aaronnorby, ian.gilman, ruthie
    
    Reviewed By: ruthie
    
    Subscribers: crabclaw
    
    Differential Revision: https://phabricator.automatic.co/D11902

--- tools/gulp/deploy.js
+++ tools/gulp/deploy.js
@@ -2,43 +2,28 @@ import assert from 'assert';
 
 import { isNumber } from 'lodash';
 import gulp from 'gulp';
-import ghPages from 'gulp-gh-pages';
 import awsPublish from 'gulp-awspublish';
 
 import config from '../../config.js';
 
-function deployGithub() {
-  try {
-    assert(config.DEPLOY_GITHUB_REMOTE_URL);
-  } catch (err) {
-    console.error('Missing github deploy environmental variables');
-    process.exit(1);
-  }
-
-  return gulp.src('dist/**')
-    .pipe(ghPages({
-      remoteUrl: config.DEPLOY_GITHUB_REMOTE_URL
-    }));
-}
-
 function deployAwsGlobs(artifactsGlob, cacheLength) {
   assert(isNumber(cacheLength));
   try {
-    assert(config.DEPLOY_AWS_BUCKET_NAME);
-    assert(config.DEPLOY_AWS_ACCESS_KEY);
-    assert(config.DEPLOY_AWS_SECRET_KEY);
-    assert(config.DEPLOY_AWS_REGION);
+    assert(config.AWS_BUCKET);
+    assert(config.AWS_ACCESS_KEY);
+    assert(config.AWS_SECRET_KEY);
+    assert(config.AWS_REGION);
   } catch (err) {
     console.error('Missing AWS deploy environmental variables');
     process.exit(1);
   }
   const publisherConf = {
     params: {
-      Bucket: config.DEPLOY_AWS_BUCKET_NAME
+      Bucket: config.AWS_BUCKET
     },
-    accessKeyId: config.DEPLOY_AWS_ACCESS_KEY,
-    secretAccessKey: config.DEPLOY_AWS_SECRET_KEY,
-    region: config.DEPLOY_AWS_REGION
+    accessKeyId: config.AWS_ACCESS_KEY,
+    secretAccessKey: config.AWS_SECRET_KEY,
+    region: config.AWS_REGION
   };
 
   const publisher = awsPublish.create(publisherConf);
@@ -50,41 +35,25 @@ function deployAwsGlobs(artifactsGlob, cacheLength) {
     .pipe(awsPublish.reporter({}));
 }
 
-function deployAws() {
-  const longTermCachePatterns = [
-    '*.html',
-    'favicon.ico',
-    'apple-touch-icon.png',
-    'apple-touch-icon-precomposed.png',
-    'robots.txt',
-    'sitemap.xml'
-  ];
+const longTermCachePatterns = [
+  '*.html',
+  'favicon.ico',
+  'apple-touch-icon.png',
+  'apple-touch-icon-precomposed.png',
+  'robots.txt',
+  'sitemap.xml'
+];
 
+gulp.task('deploy:aws:long', () => {
   const oneYear = 31536000;
   const longTermCacheGlob = `dist/**/+(${longTermCachePatterns.join('|')})`;
-  const longTermDeployStream = deployAwsGlobs(longTermCacheGlob, oneYear);
+  return deployAwsGlobs(longTermCacheGlob, oneYear);
+});
 
+gulp.task('deploy:aws:short', () => {
   const fiveMin = 300;
-  const shortTermCacheGlob = `dist/**/!(${longTermCachePatterns.join('|')}`;
-  const shortTermDeploySteam = deployAwsGlobs(shortTermCacheGlob, fiveMin);
+  const shortTermCacheGlob = `dist/**/!(${longTermCachePatterns.join('|')})`;
+  return deployAwsGlobs(shortTermCacheGlob, fiveMin);
+});
 
-  return gulp.parallel(longTermDeployStream, shortTermDeploySteam);
-}
-
-function deploy() {
-  let deployStream;
-  switch (config.DEPLOY_TARGET) {
-    case 'github':
-      deployStream = deployGithub();
-      break;
-    case 'aws':
-      deployStream = deployAws();
-      break;
-    default:
-      throw new Error('Invalid deploy target');
-  }
-
-  return deployStream;
-}
-
-gulp.task('deploy:target', deploy);
+gulp.task('deploy:target', gulp.series('deploy:aws:long', 'deploy:aws:short'));
